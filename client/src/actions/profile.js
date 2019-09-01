3508f118b185137d75c10bb63fce476425369061
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2019-09-01 15:47:56 -0600

    Profiles done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,10 +3,12 @@ import { setAlert } from './alert'
 
 import {
 	GET_PROFILE,
+	GET_PROFILES,
 	PROFILE_ERROR,
 	UPDATE_PROFILE,
 	CLEAR_PROFILE,
-	DELETE_ACCOUNT
+	DELETE_ACCOUNT,
+	GET_REPOS
 } from './types'
 
 export const getCurrentProfile = () => async (dispatch) => {
@@ -15,6 +17,64 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data
+		})
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status
+			}
+		})
+	}
+}
+
+export const getProfiles = () => async (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE })
+	try {
+		const res = await axios.get('/api/profile')
+
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data
+		})
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status
+			}
+		})
+	}
+}
+
+export const getProfileById = (userId) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/user/${userId}`)
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		})
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status
+			}
+		})
+	}
+}
+
+export const getGithubRepos = (username) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/github/${username}`)
+
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data
 		})
 	} catch (error) {
@@ -182,7 +242,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = (id) => async (dispatch) => {
 	if (window.confirm('Are you sure? This can NOT be undone!')) {
 		try {
-			const res = await axios.delete(`/api/profile`)
+			await axios.delete(`/api/profile`)
 
 			dispatch({
 				type: CLEAR_PROFILE
